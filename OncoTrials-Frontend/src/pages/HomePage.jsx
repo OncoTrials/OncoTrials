0d4a92f5f9320b0a78b46e209112853034d5c34e
jmensah365 <jmensah365@gmail.com>
2025-06-12 15:17:22 -0400

    adding footer component

--- OncoTrials-Frontend/src/pages/HomePage.jsx
+++ OncoTrials-Frontend/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import LoginDropdown from '../components/buttons/LoginDropdown'
 import RegisterDropdown from '../components/buttons/RegisterDropdown'
 import HomeNavBar from '../components/HomeNavBar'
 import SplitText from '../components/SplitText'
+import Footer from '../components/Footer'
 
 function HomePage() {
     return (
@@ -76,33 +77,7 @@ function HomePage() {
                 </div>
             </div>
 
-
-            <footer class="rounded-lg  m-4 ">
-                <div class="w-full max-w-screen mx-auto p-4 md:py-8">
-                    <div class="sm:flex sm:items-center sm:justify-between">
-                        <a href="http://localhost:5173/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                            <img src={'/vite.svg'} class="h-8" alt="Flowbite Logo" />
-                            <span class="self-center text-2xl font-semibold whitespace-nowrap text-black">OncoTrials</span>
-                        </a>
-                        <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-                            <li>
-                                <a href="#" class="hover:underline me-4 md:me-6">About</a>
-                            </li>
-                            <li>
-                                <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-                            </li>
-                            <li>
-                                <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-                            </li>
-                            <li>
-                                <a href="#" class="hover:underline">Contact</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-                    <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 OncoTrials™. All Rights Reserved.</span>
-                </div>
-            </footer>
+            <Footer/>
 
             </div>
         </>
