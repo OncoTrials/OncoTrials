6d6b60d66e2cd041388522a5f551e0d41a92ab90
jmensah365 <jmensah365@gmail.com>
2025-08-05 20:34:12 -0400

    physician/crc UI

--- OncoTrials-Frontend/src/pages/PhysicianCRCLoginPage.jsx
+++ OncoTrials-Frontend/src/pages/PhysicianCRCLoginPage.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import PhysicianCRCLoginForm from '../components/PhysicianCRCLoginForm'
+import HomeNavBar from '../components/HomeNavBar'
 
 function PhysicianCRCLoginPage() {
   return (
-    <PhysicianCRCLoginForm/>
+    <>
+      <div className=' bg-gradient-to-br from-blue-100 via-white to-indigo-100 animate-fade-down'>
+        <HomeNavBar />
+        <PhysicianCRCLoginForm />
+      </div>
+    </>
   )
 }
 
